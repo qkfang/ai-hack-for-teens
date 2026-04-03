6e6b9ddc07d0d2b7c1548185a99b7695bc719f11
Daniel Fang <qkfang@hotmail.com>
2026-04-03 18:17:40 +1100

    add

--- bicep/setup-sql-users.ps1
+++ bicep/setup-sql-users.ps1
@@ -4,3 +4,8 @@ Invoke-Sqlcmd -ServerInstance "aihack26-sql.database.windows.net" -Database "ai-
 CREATE USER [sp-demo-01] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_owner ADD MEMBER [sp-demo-01];
 "@
+
+Invoke-Sqlcmd -ServerInstance "aihack26-sql.database.windows.net" -Database "ai-hack-db-dev" -AccessToken $token -Query @"
+CREATE USER [aihack26-app] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_owner ADD MEMBER [aihack26-app];
+"@
