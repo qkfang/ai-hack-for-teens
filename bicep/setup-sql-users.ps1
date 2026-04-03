a631402b83793b310e50f52c94e29b517e01b6ff
Daniel Fang <qkfang@hotmail.com>
2026-04-03 18:23:15 +1100

    update

--- bicep/setup-sql-users.ps1
+++ bicep/setup-sql-users.ps1
@@ -1,11 +1,13 @@
 $token = (az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv)
 
 Invoke-Sqlcmd -ServerInstance "aihack26-sql.database.windows.net" -Database "ai-hack-db-dev" -AccessToken $token -Query @"
-CREATE USER [sp-demo-01] FROM EXTERNAL PROVIDER;
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'sp-demo-01')
+    CREATE USER [sp-demo-01] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_owner ADD MEMBER [sp-demo-01];
 "@
 
 Invoke-Sqlcmd -ServerInstance "aihack26-sql.database.windows.net" -Database "ai-hack-db-dev" -AccessToken $token -Query @"
-CREATE USER [aihack26-app] FROM EXTERNAL PROVIDER;
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'aihack26-app')
+    CREATE USER [aihack26-app] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_owner ADD MEMBER [aihack26-app];
 "@
