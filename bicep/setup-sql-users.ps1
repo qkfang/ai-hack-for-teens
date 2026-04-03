b221dbe4c12115a345bcca9125ceb761ca0ae184
Daniel Fang <qkfang@hotmail.com>
2026-04-03 18:01:29 +1100

    add

---
+++ bicep/setup-sql-users.ps1
@@ -0,0 +1,6 @@
+$token = (az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv)
+
+Invoke-Sqlcmd -ServerInstance "aihack26-sql.database.windows.net" -Database "ai-hack-db-dev" -AccessToken $token -Query @"
+CREATE USER [sp-demo-01] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_owner ADD MEMBER [sp-demo-01];
+"@
